d6d4990048c1365530b6cfc426a37c03c4ab064a
Connor Onweller <onweller@ubuntu-1604.eecis.udel.edu>
2019-05-05 23:27:40 -0400

    it works

--- t_lib.c
+++ t_lib.c
@@ -5,7 +5,6 @@
 //ucontext_t *ready;
 tcb *running;
 tcb *ready;
-tcb *head;
 
 void t_yield()
 {
@@ -20,7 +19,18 @@ void t_yield()
 		tmp2->next = tmp;
 		ready = ready->next;
 		running->next = NULL;
-		swapcontext(ready->thread_context, running->thread_context);
+		tmp2 = ready;
+		if (tmp2 !=NULL)
+			while (tmp2->next != NULL) 
+				tmp2 = tmp2->next;
+		swapcontext(tmp2->thread_context, running->thread_context);
+
+		/* tmp = ready; */
+		/* while (tmp != NULL){ */
+		/* 	printf("%d -> ", tmp->thread_id); */
+		/* 	tmp = tmp->next; */
+		/* } */
+		/* printf("NULL\n"); */
 	}
 }
 
@@ -39,16 +49,15 @@ void t_init()
 
 int t_create(void (*fct)(int), int id, int pri)
 {
+	/* printf("created %d\n", id); */
 	size_t sz = 0x10000;
 	ucontext_t *uc;
 	uc = (ucontext_t *) malloc(sizeof(ucontext_t));
 
 	getcontext(uc);
-	/***
-	  uc->uc_stack.ss_sp = mmap(0, sz,
-	  PROT_READ | PROT_WRITE | PROT_EXEC,
-	  MAP_PRIVATE | MAP_ANON, -1, 0);
-	 ***/
+	  /* uc->uc_stack.ss_sp = mmap(0, sz, */
+	  /* PROT_READ | PROT_WRITE | PROT_EXEC, */
+	  /* MAP_PRIVATE | MAP_ANON, -1, 0); */
 	uc->uc_stack.ss_sp = malloc(sz);  /* new statement */
 	uc->uc_stack.ss_size = sz;
 	uc->uc_stack.ss_flags = 0;
@@ -69,21 +78,51 @@ int t_create(void (*fct)(int), int id, int pri)
 		}
 		tmp->next = new_tcb;
 	}
+	/* tmp = ready; */
+	/* while (tmp != NULL){ */
+	/* 	printf("%d -> ", tmp->thread_id); */
+	/* 	tmp = tmp->next; */
+	/* } */
+	/* printf("NULL\n"); */
 }
 
 
 void t_shutdown(void) {
-	tcb *tmp = ready;
-	while (tmp->next != NULL) {
-		tcb *last = tmp;
-		tmp = tmp->next;
-		free(last);
+
+	if (ready != NULL) {
+		tcb *tmp = ready;
+		while (tmp->next != NULL) {
+			tcb *last = tmp;
+			tmp = tmp->next;
+			tcb_free(last);
+			free(last);
+		}
+		free(tmp);
 	}
-	free(tmp);
+	/* free(running->thread_context); */
+	tcb_free(running);
 	free(running);
 }
 
 void t_terminate(void) {
-	return;
+	/* tcb *tmp = running; */
+	/* running->next = NULL; */
+	/* running = ready; */
+	/* ready = ready->next; */
+	/* /1* running->next = NULL; *1/ */
+	/* setcontext(running->thread_context); */
+	/* tcb_free(tmp); */
+	/* free(tmp); */
+	running->next = NULL;
+	tcb_free(running);
+	free(running);
+	running = ready;
+	ready = ready->next;
+	running->next = NULL;
+	setcontext(running->thread_context);
 }
-void tcb_free(tcb *thread);
+void tcb_free(tcb *thread) {
+	free(thread->thread_context->uc_stack.ss_sp);
+	/* free(thread->thread_context->uc_stack); */
+	free(thread->thread_context);
+}
